f8375ee2b49b5b93e7a14a46d54b2360e250b659
Евгений Агаджанов <geniy@live.ru>
2026-02-17 15:36:38 +0300

    Add_new_feature

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.224.46', '127.0.0.1', 'localhost', 'taski-taski.ddns.net']
+ALLOWED_HOSTS = [
+    '158.160.224.46', '127.0.0.1', 'localhost', 'taski-taski.ddns.net'
+]
 
 
 # Application definition
